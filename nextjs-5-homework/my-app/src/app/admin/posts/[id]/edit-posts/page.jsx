11d7645881b60693b7438919050043ed879fe69c
hadiseh-a <hadisea82@gmail.com>
2025-11-03 10:08:14 +0330

    ruined edit

---
+++ nextjs-5-homework/my-app/src/app/admin/posts/[id]/edit-posts/page.jsx
@@ -0,0 +1,12 @@
+// "use client"
+// import CardForm from "@/components/CardForm";
+// import React, { useEffect, useState } from "react";
+
+// export default function page({params}) {
+//     const [item,setItem]=useState()
+//     useEffect(()=>{
+//         fetch(`http://localhost:3000/posts/${params.id}`)
+//     },[params.id])
+//   return <CardForm content={'Post'} initialData={ title={item.title},
+//         description={item.body}}/>;
+// }
